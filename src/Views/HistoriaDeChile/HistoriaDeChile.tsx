88e0631c0dc477ebfb76977cd94960f538af7992
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2021-11-03 20:43:34 -0300

    refactor metadata management

--- src/Views/HistoriaDeChile/HistoriaDeChile.tsx
+++ src/Views/HistoriaDeChile/HistoriaDeChile.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 
 import { Grid, Typography } from "@mui/material";
 
-import { linksMetadata } from "./Utils/linksMetadata";
+import { cardsMetadata } from "./Utils/linksMetadata";
 
 import SectionCard from "../../Components/SectionCard";
 
@@ -14,58 +14,15 @@ const HistoriaDeChile: FC = () => (
       </Typography>
     </Grid>
 
-    <Grid item xs={6}>
-      <SectionCard
-        title="Congreso nacional de Chile"
-        linksMetadata={linksMetadata.congresoNacional}
-      />
-    </Grid>
-
-    <Grid item xs={6}>
-      <SectionCard
-        title="Separación Iglesia y Estado"
-        content={[
-          "La Constitución de 1833 dice que: “La religión de la República de Chile es la católica, apostólica, romana; con exclusión del ejercicio público de cualquiera otra”. A lo largo del siglo llegaron inmigrantes con credos distintos al católico, anglicanismo y luteranismo por ejemplo. Esto, más el impulso de un sector de la sociedad de ideas liberales, en dirección hacia un secularismo llevó por ejemplo a la creación de la Ley Interpretativa de 1865",
-          "Esta estableció que quienes no profesan la religión católica podían practicar su culto al interior de edificios de propiedad particular. Y que los extranjeros no católicos, llamados disidentes, podían sostener escuelas privadas para la enseñanza de sus propios hijos en la doctrina de sus religiones.",
-          "60 años después con la Constitución de 1925 se estableció la separación de la Iglesia con el Estado. La Carta es muy similar a la actual Constitución. La Comisión Ortúzar, durante los años 70, señaló relevante mantener lo establecido en este punto.",
-        ]}
-      />
-    </Grid>
-
-    <Grid item xs={6}>
-      <SectionCard
-        title="Privatización de las empresas estateles"
-        linksMetadata={linksMetadata.privatizacionEmpresasEstateles}
-      />
-    </Grid>
-
-    <Grid item xs={6}>
-      <SectionCard
-        title="Pueblos originarios"
-        linksMetadata={linksMetadata.pueblosOriginarios}
-      />
-    </Grid>
-
-    <Grid item xs={6}>
-      <SectionCard
-        title="Sufragio Femenino en Chile"
-        content={[
-          "El Sufragio femenino en Chile hace referencia al derecho a votos de las mujeres de este país, que comienza con las elecciones municipales del año 1935 logrando en el año 1949 el Sufragio femenino irrestricto",
-          "Se considera como punto de partida el Decreto Amunátegui del 6 de noviembre de 1877, que habilitó a la mujer a realizar estudios universitarios. Surgen poco a poco las profesionales pioneras de distintas áreas, que conceptualizan la inferioridad en que se encuentra la mujer.",
-        ]}
-        linksMetadata={linksMetadata.sugrafioFemenino}
-      />
-    </Grid>
-
-    <Grid item xs={6}>
-      <SectionCard
-        title="Régimen Parlamentario"
-        content={[
-          "La República Parlamentaria o Régimen Parlamentario es un período de la historia de Chile que se extendió entre 1891 —cuando estalló la Guerra civil— y en 1925 —cuando se produjo el Golpe de Estado de 1924, que implicó la clausura del Congreso, y la promulgación de la Constitución de 1925, que estableció una República presidencial",
-        ]}
-        linksMetadata={linksMetadata.regimenParlamentario}
-      />
-    </Grid>
+    {cardsMetadata.map(({ title, links, content }) =>
+      title ? (
+        <Grid item xs={12} md={6}>
+          <SectionCard title={title} content={content} linksMetadata={links} />
+        </Grid>
+      ) : (
+        <></>
+      )
+    )}
   </Grid>
 );
 
--- src/Views/HistoriaDeChile/Utils/linksMetadata.tsx
+++ src/Views/HistoriaDeChile/Utils/linksMetadata.tsx
@@ -3,51 +3,212 @@ export interface ILinkMetadata {
   title: string;
 }
 
-export const linksMetadata: Record<string, ILinkMetadata[]> = {
-  congresoNacional: [
-    {
-      url: "http://www.memoriachilena.gob.cl/602/w3-article-596.html",
-      title: "El Congreso Nacional de Chile (1811-1925)",
-    },
-    {
-      url: "https://www.senado.cl/notas-de-contexto-historico/los-quiebres-institucionales-que-significaron-el-fin-de-la-democracia-y",
-      title:
-        "Los quiebres institucionales que significaron el fin de la democracia y el cierre del Congreso",
-    },
-  ],
-  privatizacionEmpresasEstateles: [
-    {
-      url: "https://radio.uchile.cl/2016/05/20/el-turbio-proceso-de-privatizacion-de-empresas-estatales-durante-la-dictadura/",
-      title:
-        "El irregular proceso de privatización de empresas estatales durante la dictadura",
-    },
-    {
-      url: "https://www.eltiempo.com/archivo/documento/MAM-196956",
-      title: "PRIVATIZACIÓN EN CHILE: UN BUEN EJEMPLO",
-    },
-  ],
-  pueblosOriginarios: [
-    {
-      title: "Cultura y pueblos originarios",
-      url: "https://chile.gob.cl/iran/sobre-chile/cultura-y-pueblos-originarios",
-    },
-  ],
-  sugrafioFemenino: [
-    {
-      title: "Sufragio femenino en Chile",
-      url: "https://es.wikipedia.org/wiki/Sufragio_femenino_en_Chile",
-    },
-  ],
-  regimenParlamentario: [
-    {
-      title: "República Parlamentaria (Chile)",
-      url: "https://es.wikipedia.org/wiki/Rep%C3%BAblica_Parlamentaria_(Chile)",
-    },
-  ],
-  // name: [
-  //   {
-  //     title: "",
-  //     url: "",
-  //   },
-  // ],
-};
+interface ICardMetadata {
+  title: string;
+  links?: ILinkMetadata[];
+  content?: string[];
+}
+
+export const cardsMetadata: ICardMetadata[] = [
+  {
+    title: "Congreso nacional de Chile",
+    links: [
+      {
+        url: "http://www.memoriachilena.gob.cl/602/w3-article-596.html",
+        title: "El Congreso Nacional de Chile (1811-1925)",
+      },
+      {
+        url: "https://www.senado.cl/notas-de-contexto-historico/los-quiebres-institucionales-que-significaron-el-fin-de-la-democracia-y",
+        title:
+          "Los quiebres institucionales que significaron el fin de la democracia y el cierre del Congreso",
+      },
+    ],
+  },
+  {
+    title: "Separación Iglesia y Estado",
+    content: [
+      "La Constitución de 1833 dice que: “La religión de la República de Chile es la católica, apostólica, romana; con exclusión del ejercicio público de cualquiera otra”. A lo largo del siglo llegaron inmigrantes con credos distintos al católico, anglicanismo y luteranismo por ejemplo. Esto, más el impulso de un sector de la sociedad de ideas liberales, en dirección hacia un secularismo llevó por ejemplo a la creación de la Ley Interpretativa de 1865",
+      "Esta estableció que quienes no profesan la religión católica podían practicar su culto al interior de edificios de propiedad particular. Y que los extranjeros no católicos, llamados disidentes, podían sostener escuelas privadas para la enseñanza de sus propios hijos en la doctrina de sus religiones.",
+      "60 años después con la Constitución de 1925 se estableció la separación de la Iglesia con el Estado. La Carta es muy similar a la actual Constitución. La Comisión Ortúzar, durante los años 70, señaló relevante mantener lo establecido en este punto.",
+    ],
+  },
+  {
+    title: "Privatización de las empresas estatales",
+    links: [
+      {
+        url: "https://radio.uchile.cl/2016/05/20/el-turbio-proceso-de-privatizacion-de-empresas-estatales-durante-la-dictadura/",
+        title:
+          "El irregular proceso de privatización de empresas estatales durante la dictadura",
+      },
+      {
+        url: "https://www.eltiempo.com/archivo/documento/MAM-196956",
+        title: "PRIVATIZACIÓN EN CHILE: UN BUEN EJEMPLO",
+      },
+    ],
+  },
+  {
+    title: "Pueblos originarios",
+    links: [
+      {
+        title: "Cultura y pueblos originarios",
+        url: "https://chile.gob.cl/iran/sobre-chile/cultura-y-pueblos-originarios",
+      },
+      {
+        title: "El pueblo aymara",
+        url: "http://www.memoriachilena.gob.cl/602/w3-article-605.html",
+      },
+    ],
+  },
+  {
+    title: "Sufragio Femenino en Chile",
+    content: [
+      "El Sufragio femenino en Chile hace referencia al derecho a votos de las mujeres de este país, que comienza con las elecciones municipales del año 1935 logrando en el año 1949 el Sufragio femenino irrestricto",
+      "Se considera como punto de partida el Decreto Amunátegui del 6 de noviembre de 1877, que habilitó a la mujer a realizar estudios universitarios. Surgen poco a poco las profesionales pioneras de distintas áreas, que conceptualizan la inferioridad en que se encuentra la mujer.",
+    ],
+    links: [
+      {
+        title: "Sufragio femenino en Chile",
+        url: "https://es.wikipedia.org/wiki/Sufragio_femenino_en_Chile",
+      },
+      {
+        title: "Sufragio femenino universal",
+        url: "http://www.memoriachilena.gob.cl/602/w3-article-96054.html",
+      },
+    ],
+  },
+  {
+    title: "Régimen Parlamentario",
+    content: [
+      "La República Parlamentaria o Régimen Parlamentario es un período de la historia de Chile que se extendió entre 1891 —cuando estalló la Guerra civil— y en 1925 —cuando se produjo el Golpe de Estado de 1924, que implicó la clausura del Congreso, y la promulgación de la Constitución de 1925, que estableció una República presidencial",
+    ],
+    links: [
+      {
+        title: "República Parlamentaria (Chile)",
+        url: "https://es.wikipedia.org/wiki/Rep%C3%BAblica_Parlamentaria_(Chile)",
+      },
+    ],
+  },
+  {
+    title: "Rotativa Ministerial",
+    content: [
+      "La rotativa ministerial fue una práctica política utilizada en Chile, principalmente en el período conocido como la República Parlamentaria (1891-1925), que consistía en la inestabilidad de los gabinetes nombrados por el presidente de la República, debido a las facultades que poseía el Congreso Nacional en cuanto a la interpelación de los ministros de Estado. De ahí que existía una continua «rotativa» o cambio de ministros, por lo que no era raro que el número de gabinetes por Presidente fuera siempre superior a 10.",
+    ],
+    links: [
+      {
+        title: "Rotativa ministerial",
+        url: "https://es.wikipedia.org/wiki/Rotativa_ministerial",
+      },
+    ],
+  },
+  {
+    title: "Sufragio universal",
+    content: [],
+    links: [
+      {
+        title:
+          "Fin del sufragio censitario y establecimiento del voto secreto.",
+        url: "https://www.bcn.cl/historiapolitica/elecciones/detalle_eleccion?handle=10221.1/62727&periodo=1833-1891",
+      },
+      {
+        title: "Elecciones, sufragio y democracia en Chile (1810-2012)",
+        url: "http://www.memoriachilena.gob.cl/602/w3-article-3382.html",
+      },
+    ],
+  },
+  {
+    title: "Constitucion chilena",
+    content: [],
+    links: [
+      {
+        title: "Evolución constitucional",
+        url: "https://www.bcn.cl/formacioncivica/constituciones.html",
+      },
+      {
+        title: "Constitución de 1925",
+        url: "http://www.memoriachilena.gob.cl/602/w3-article-96990.html",
+      },
+    ],
+  },
+  {
+    title: "República autoritaria",
+    content: [],
+    links: [
+      {
+        title: "República Conservadora (Chile)",
+        url: "https://es.wikipedia.org/wiki/Rep%C3%BAblica_Conservadora_(Chile)",
+      },
+      {
+        title:
+          "Periodo 1833-1891. República oligárquica y Guerra Civil de 1891",
+        url: "https://www.bcn.cl/historiapolitica/hitos_periodo/detalle_periodo.html?filtros=1,2,3,4,5,6&per=1833-1891&pagina=5&K=1",
+      },
+    ],
+  },
+  {
+    title: "Guerra Civil",
+    content: [],
+    links: [
+      {
+        title: "La Guerra Civil de 1891",
+        url: "http://www.memoriachilena.gob.cl/602/w3-article-706.html",
+      },
+    ],
+  },
+  {
+    title: "Guerra del Pacífico",
+    content: [],
+    links: [
+      {
+        title: "Guerra del Pacífico",
+        url: "https://es.wikipedia.org/wiki/Guerra_del_Pac%C3%ADfico",
+      },
+      {
+        title: "El impacto de la guerra del Pacífico (1879-1929)",
+        url: "http://www.memoriachilena.gob.cl/602/w3-article-100610.html",
+      },
+    ],
+  },
+  {
+    title: "Reformas estructurales",
+    content: [],
+    links: [
+      {
+        title: "Reformas estructurales",
+        url: "http://www.memoriachilena.gob.cl/602/w3-article-95158.html",
+      },
+    ],
+  },
+  {
+    title: "Independencia de Chile",
+    content: [],
+    links: [
+      {
+        title: "Independencia de Chile",
+        url: "https://es.wikipedia.org/wiki/Independencia_de_Chile",
+      },
+      {
+        title:
+          "Independencia de Chile: por qué el país la celebra en septiembre y no el 12 de febrero, cuando realmente ocurrió",
+        url: "https://www.bbc.com/mundo/noticias-america-latina-47205798",
+      },
+    ],
+  },
+  {
+    title: "",
+    content: [],
+    links: [
+      {
+        title: "",
+        url: "",
+      },
+      {
+        title: "",
+        url: "",
+      },
+      {
+        title: "",
+        url: "",
+      },
+    ],
+  },
+];
